276c63bd10e4fd6b21f4c855dfc3e1c780a4d046
SeungHyuk Kim <hyuk1262@gmail.com>
2025-07-21 15:46:16 +0900

    update

--- src/main/java/com/shop/shop/service/MemberService.java
+++ src/main/java/com/shop/shop/service/MemberService.java
@@ -11,6 +11,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @Transactional // 에러 발생시 콜백
 @RequiredArgsConstructor // final, @NotNull 붙은 필드에 생성자 생성
@@ -23,13 +25,12 @@ public class MemberService implements UserDetailsService { // MemberService 가 
         return memberRepository.save(member);
     }
 
+    @Transactional(readOnly = true)
     private void validateDuplicateMember(Member member) { // 가입된 회원의 경우 예외 발생
-        Member findMember = memberRepository.findByEmail(member.getEmail())
-                .orElseThrow(() -> new EntityNotFoundException("해당 이메일의 회원을 찾을 수 없습니다."));
-
-        if (findMember != null){
-                throw new IllegalStateException("이미 가입된 회원입니다.");
-            }
+        Optional<Member> findMember = memberRepository.findByEmail(member.getEmail());
+        if(findMember.isPresent()){ // 해당 이메일을 가진 회원이 이미 존재한다면
+            throw new IllegalStateException("이미 가입된 회원입니다."); // 중복 회원이므로 예외 발생
+        }
     }
 
     @Override
