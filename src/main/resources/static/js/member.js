43884fb5cf45f81d39deea2627889e430b05468f
SeungHyuk Kim <hyuk1262@gmail.com>
2025-11-27 16:50:59 +0900

    update

--- src/main/resources/static/js/member.js
+++ src/main/resources/static/js/member.js
@@ -1,7 +1,11 @@
 // memberForm.html
-$(document).ready(function(){
+
+$(document).ready(function () {
     var errorMessage = [[${errorMessage}]];
-    if(errorMessage != null){
+    if (errorMessage != null) {
         alert(errorMessage);
     }
+
+    // 주소 검색 버튼(JS 통일)
+    $("#postcodify_search_button").postcodifyPopUp();
 });
